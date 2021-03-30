6997ed2f202127eea6f10cb8b679da75c804094f
Smriti Singh <rekha.aol17@gmail.com>
2021-03-30 18:13:19 +0530

    dummy removed

--- src/app/services/makeup/makeup.component.ts
+++ src/app/services/makeup/makeup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import { AppointmentModelComponent } from 'src/app/models/appointment-model/appointment-model.component';
 
 @Component({
   selector: 'sm-makeup',
@@ -8,13 +10,25 @@ import { TabsetComponent } from 'ngx-bootstrap/tabs';
 })
 export class MakeupComponent implements OnInit {
   @ViewChild('staticTabs', { static: false }) staticTabs: TabsetComponent;
+  bsModalRef: BsModalRef;
 
-  constructor() {}
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit(): void {}
 
   selectTab(tabId: number, event) {
     event.preventDefault();
     this.staticTabs.tabs[tabId].active = true;
+  }
+
+  bookAppointment(title) {
+    const initialState: any = {
+      title,
+    };
+    this.bsModalRef = this.modalService.show(AppointmentModelComponent, {
+      initialState,
+      class: 'modalCommon modal-lg',
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
   }
 }
